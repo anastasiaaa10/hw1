2a45cb9de15c3e1e0c37d7c0138b932be499abb6
anastasiaaa10 <144059501+anastasiaaa10@users.noreply.github.com>
2024-05-27 11:33:11 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
+from setuptools import setup
+
+       setup(
+           name='your_package_name',
+           install_requires=[
+               'aiohttp',
+               'aiofiles',
+           ],
+       )
 
